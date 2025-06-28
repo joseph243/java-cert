01f4bdbd5bc7a141dad6420b4961c900ea21f301
Joseph Vanderzwart <josephvanderzwart@protonmail.com>
2025-06-27 20:28:08 -0600

    first

---
+++ src/main/java/org/example/Library.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Library {
+    ArrayList<Book> books;
+
+    public Library() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public void removeBook(Book book) {
+        books.removeIf(book::equals);
+    }
+}
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,51 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        String HELP = "help";
+        String LIST = "list";
+        String ADD_BOOK = "add";
+
+        boolean running = true;
+
+        while (running)
+        {
+            Library library = new Library();
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("enter your command: ");
+            String input = scanner.nextLine();
+            libraryWait();
+
+
+
+            if (input.equals("help"))
+            {
+                System.out.println("command list: ");
+                System.out.println(HELP + ": this help menu.");
+                System.out.println(LIST + ": list books.");
+                System.out.println(ADD_BOOK + ": add a book");
+            }
+            if (input.equals("exit") || input.equals("quit") || input.equals("q"))
+            {
+                System.out.println("Exiting ...");
+                libraryWait();
+                running = false;
+            }
+        }
+    }
+
+    public static void libraryWait()
+    {
+        try
+        {
+            Thread.sleep(500);
+        }
+        catch (Exception e)
+        {
+            //do nothing
+        }
     }
 }
